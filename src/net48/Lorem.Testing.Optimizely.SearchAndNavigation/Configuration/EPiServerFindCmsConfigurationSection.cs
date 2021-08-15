064ee5f75c4ea3d94b8536f67744b03d7d8b63c0
loremipsumdonec <mikael.karlsson@tiff.se>
2021-08-15 09:53:49 +0200

    Support for Search and Navigation

---
+++ src/net48/Lorem.Testing.Optimizely.SearchAndNavigation/Configuration/EPiServerFindCmsConfigurationSection.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+
+namespace Lorem.Testing.Optimizely.SearchAndNavigation.Configuration
+{
+    public class EPiServerFindCmsConfigurationSection
+        : EPiServer.Find.Cms.Configuration
+    {
+        public EPiServerFindCmsConfigurationSection()
+        {
+        }
+
+        public EPiServerFindCmsConfigurationSection(ConfigurationSection source)
+        {
+            Properties.Add(new ConfigurationProperty("disableEventedIndexing", typeof(bool)));
+            Properties.Add(new ConfigurationProperty("disableScheduledPageQueue", typeof(bool)));
+            Properties.Add(new ConfigurationProperty("deleteLanguageRoutingDuplicatesOnIndex", typeof(bool)));
+
+            if (source != null)
+            {
+                Reset(source);
+            }
+        }
+
+        public new bool DisableEventedIndexing
+        {
+            get
+            {
+                return base.DisableEventedIndexing;
+            }
+            set
+            {
+                SetPropertyValue(Properties["disableEventedIndexing"], value, true);
+            }
+        }
+
+        public new bool DisableScheduledPageQueue
+        {
+            get
+            {
+                return base.DisableScheduledPageQueue;
+            }
+            set
+            {
+                SetPropertyValue(Properties["disableScheduledPageQueue"], value, true);
+            }
+        }
+
+        public new bool DeleteLanguageRoutingDuplicatesOnIndex
+        {
+            get
+            {
+                return base.DeleteLanguageRoutingDuplicatesOnIndex;
+            }
+            set
+            {
+                SetPropertyValue(Properties["deleteLanguageRoutingDuplicatesOnIndex"], value, true);
+            }
+        }
+    }
+}
---
+++ src/net48/Lorem.Testing.Optimizely.SearchAndNavigation/Modules/SearchAndNavigationTestModule.cs
@@ -0,0 +1,64 @@
+﻿using EPiServer.Framework.Configuration;
+using EPiServer.Framework.Initialization;
+using Lorem.Testing.Optimizely.CMS.Commands;
+using Lorem.Testing.Optimizely.SearchAndNavigation.Configuration;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Lorem.Testing.Optimizely.CMS.Modules
+{
+    public class SearchAndNavigationTestModule
+        : ITestModule
+    {
+        public void BeforeInitialize(InitializationEngine engine)
+        {
+            CopyConfigurationFromWebConfigToAppConfig();
+        }
+
+        public void AfterInitialize(InitializationEngine engine)
+        {
+        }
+
+        private void CopyConfigurationFromWebConfigToAppConfig()
+        {
+            CopyEpiserverFindConfigurationSection();
+            CopyEPiServerFindCmsConfigurationSection();
+        }
+
+        private void CopyEpiserverFindConfigurationSection()
+        {
+            FileConfigurationSource instance = (FileConfigurationSource)ConfigurationSource.Instance;
+
+            var episerverFindSection = (EPiServer.Find.Configuration)instance.ConfigurationInstance.GetSection("episerver.find");
+
+            var currentAppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            currentAppConfig.AppSettings.Settings.Add("episerver:FindServiceUrl", episerverFindSection.ServiceUrl);
+            currentAppConfig.AppSettings.Settings.Add("episerver:FindDefaultIndex", episerverFindSection.DefaultIndex);
+
+            currentAppConfig.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private void CopyEPiServerFindCmsConfigurationSection()
+        {
+            FileConfigurationSource instance = (FileConfigurationSource)ConfigurationSource.Instance;
+            var currentAppConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            var episerverFindCmsSection = new EPiServerFindCmsConfigurationSection(instance.ConfigurationInstance.GetSection("episerver.find.cms"));
+            episerverFindCmsSection.DisableEventedIndexing = true;
+            episerverFindCmsSection.DisableScheduledPageQueue = true;
+
+            currentAppConfig.Sections.Remove("episerver.find.cms");
+            currentAppConfig.Sections.Add("episerver.find.cms", episerverFindCmsSection);
+            currentAppConfig.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("episerver.find.cms");
+        }
+
+        public IEnumerable<IClearCommand> Reset()
+        {
+            return new List<IClearCommand>();
+        }
+    }
+}
